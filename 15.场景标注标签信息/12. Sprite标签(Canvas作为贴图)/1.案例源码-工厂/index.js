289f25b00823ef4f87cb5e620c53e7dce30668bd
nanshu <1085254227@qq.com>
2026-01-15 15:26:41 +0800

    15-12Sprite标签(Canvas作为贴图)

---
+++ 15.场景标注标签信息/12. Sprite标签(Canvas作为贴图)/1.案例源码-工厂/index.js
@@ -0,0 +1,99 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// 引入CSS3渲染器CSS3DRenderer
+import { CSS3DRenderer } from "three/addons/renderers/CSS3DRenderer.js";
+// 引入CSS3精灵模型对象CSS3DSprite
+import { CSS3DSprite } from "three/addons/renderers/CSS3DRenderer.js";
+
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+// 引入渲染器通道RenderPass
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+// 引入OutlinePass通道
+import { OutlinePass } from "three/addons/postprocessing/OutlinePass.js";
+// 伽马校正后处理Shader
+import { GammaCorrectionShader } from "three/addons/shaders/GammaCorrectionShader.js";
+// ShaderPass功能：使用后处理Shader创建后处理通道
+import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
+import model from "./model.js"; //模型对象
+import { SMAAPass } from "three/addons/postprocessing/SMAAPass.js";
+
+//场景
+const scene = new THREE.Scene();
+
+scene.add(model); //模型对象添加到场景中
+
+//辅助观察的坐标系
+const axesHelper = new THREE.AxesHelper(100);
+scene.add(axesHelper);
+
+//光源设置
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(400, 200, 300);
+scene.add(directionalLight);
+const ambient = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambient);
+
+//渲染器和相机
+const width = window.innerWidth;
+const height = window.innerHeight;
+const camera = new THREE.PerspectiveCamera(30, width / height, 1, 3000);
+camera.position.set(100, 40, 80);
+camera.lookAt(0, 0, 0);
+
+const renderer = new THREE.WebGLRenderer({
+  antialias: true, //抗锯齿
+});
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
+
+// 创建后处理对象EffectComposer，WebGL渲染器作为参数
+const composer = new EffectComposer(renderer);
+//创建一个渲染器通道，场景和相机作为参数
+const renderPass = new RenderPass(scene, camera);
+composer.addPass(renderPass);
+
+// OutlinePass第一个参数v2的尺寸和canvas画布保持一致
+const v2 = new THREE.Vector2(window.innerWidth, window.innerHeight);
+const outlinePass = new OutlinePass(v2, scene, camera);
+outlinePass.visibleEdgeColor.set(0x00ffff);
+outlinePass.edgeThickness = 4;
+outlinePass.edgeStrength = 6;
+composer.addPass(outlinePass);
+
+// 创建伽马校正通道
+const gammaPass = new ShaderPass(GammaCorrectionShader);
+composer.addPass(gammaPass);
+// 抗锯齿
+const pixlRatio = window.devicePixelRatio;
+const smaaPass = new SMAAPass(width * pixlRatio, height * pixlRatio);
+composer.addPass(smaaPass);
+
+// 创建一个CSS3渲染器CSS3DRenderer
+const css3Renderer = new CSS3DRenderer();
+css3Renderer.setSize(width, height);
+// HTML标签<div id="tag"></div>外面父元素叠加到canvas画布上且重合
+css3Renderer.domElement.style.position = "absolute";
+css3Renderer.domElement.style.top = "0px";
+//设置.pointerEvents=none，解决HTML元素标签对threejs canvas画布鼠标事件的遮挡
+css3Renderer.domElement.style.pointerEvents = "none";
+document.body.appendChild(css3Renderer.domElement);
+
+// 渲染循环
+function render() {
+  css3Renderer.render(scene, camera);
+  composer.render();
+  // renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+render();
+
+// 设置相机控件轨道控制器OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+// 画布跟随窗口变化
+window.onresize = function () {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // HTML标签css3Renderer.domElement尺寸重新设置
+  css3Renderer.setSize(width, height);
+  camera.updateProjectionMatrix();
+};
